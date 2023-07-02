bf5ce1d15ec7553c853af8e863bbd682bf619b3b
Ruslan <abishev2012@gmail.com>
2023-07-02 16:09:32 +0500

    changes

--- script.js
+++ script.js
@@ -1,110 +1,84 @@
 class MyArray {
-  constructor() {
-    // Инициализация пустого массива и свойства birthday как текущую дату
+  #birthday; constructor()
+   {
     this.arr = [];
     let birthday = new Date();
-    
-    // Приватное поле birthday
-    this.getBirthday = function() {
+    this.#birthday = function() {
       return birthday;
     }
   }
   
-    getString(separator) {
-      // Объединение элементов массива в строку с использованием разделителя
-      return this.arr.join(separator);
-    }
+  getString(separator) {
+    return this.arr.join(separator);
+  }
   
-    getHtml(type) {
-      let html = '';
-  
-      if (type) {
-        // Если type равен true, то добавляем теги <ul> и <li> для форматирования элементов массива
-        html += '<ul>';
-  
-        for (let i = 0; i < this.arr.length; i++) {
-          // Добавляем каждый элемент массива внутри тега <li>
-          html += `<li>${this.arr[i]}</li>`;
-        }
-  
-        html += '</ul>';
-      } else {
-        // Если type равен false, то выводим элементы массива без использования тегов
-        for (let i = 0; i < this.arr.length; i++) {
-          html += `${this.arr[i]} `;
-        }
-      }
-  
-      return html;
-    }
-  
-    sort() {
-      // Сортировка элементов массива в порядке возрастания
+  getHtml(type) {
+    let html = '';
+
+    if (type) {
+      html += '<ol>';
       for (let i = 0; i < this.arr.length; i++) {
-        for (let j = 0; j < this.arr.length - i - 1; j++) {
-          if (this.arr[j] > this.arr[j + 1]) {
-            let temp = this.arr[j];
-            this.arr[j] = this.arr[j + 1];
-            this.arr[j + 1] = temp;
-          }
+        html += `<li>${this.arr[i]}</li>`;
+      }
+      html += '</ol>';
+    } else {
+      html += '<ul>';
+      for (let i = 0; i < this.arr.length; i++) {
+        html += `<li>${this.arr[i]}</li>`;
+      }
+      html += '</ul>';
+    }
+    
+    return html;
+  }
+  
+  sort() {
+    for (let i = 0; i < this.arr.length; i++) {
+      for (let j = 0; j < this.arr.length - i - 1; j++) {
+        if (this.arr[j] > this.arr[j + 1]) {
+          let temp = this.arr[j];
+          this.arr[j] = this.arr[j + 1];
+          this.arr[j + 1] = temp;
         }
       }
     }
   }
-  
-  // Создание нового объекта класса MyArray
-  const extendedArray = new MyArray();
-  
-  // Присваивание массива [9, 1, 8, 5, 3, 7, 15, 54, 27, 6] свойству arr объекта extendedArray
-  extendedArray.arr = [9, 1, 8, 5, 3, 7, 15, 54, 27, 6];
-  
-  // Вывод строки, полученной из объединения элементов массива с разделителем ', '
-  console.log('Вывод строки, полученной из объединения элементов массива с разделителем ","\n',extendedArray.getString(', '));
+}
 
-  
-  // Вывод строки, содержащей элементы массива в формате HTML с использованием тегов <li>
-  console.log('Вывод строки, содержащей элементы массива в формате HTML с использованием тегов <li>\n', extendedArray.getHtml(true));
+const extendedArray = new MyArray();
+extendedArray.arr = [9, 1, 8, 5, 3, 7, 15, 54, 27, 6];
 
-   // Получение элемента с id "output" из DOM
-   const outputElement = document.getElementById('output');
+console.log('Вывод строки, полученной из объединения элементов массива с разделителем ","\n', extendedArray.getString(', '));
+console.log('Вывод строки, содержащей элементы массива в формате HTML с использованием тегов \n', extendedArray.getHtml(true));
 
-     // Присвоение элементу outputElement значения, возвращаемого методами getString() и getHtml() объекта extendedArray
-     outputElement.innerHTML = 'Вывод строки, полученной из объединения отсортированных элементов массива с разделителем ","\n<br>' + extendedArray.getString(', ')+ "\n";
-     outputElement.innerHTML += '\n<br>Вывод строки, содержащей элементы массива в формате HTML с использованием тегов &lt;li&gt;<br>' + 
-     extendedArray.getHtml(true);
+extendedArray.sort();
 
-      // Сортировка элементов массива
-  extendedArray.sort();
+console.log('Вывод строки, полученной из объединения отсортированных элементов массива с разделителем ","\n', extendedArray.getString(', '));
 
-  // Вывод строки, полученной из объединения отсортированных элементов массива с разделителем ', '
-  console.log( 'Вывод строки, полученной из объединения отсортированных элементов массива с разделителем ","\n',extendedArray.getString(', '));
-  
-  // Вывод строки, содержащей элементы массива без использования тегов
-  console.log(' Вывод строки, содержащей элементы массива без использования тегов \n',extendedArray.getHtml(false));
-  
-
-  class Person {
-    constructor(name, dateOfBirth, address) {
-      // Инициализация свойств объекта Person
-      this.name = name;
-      this.dateOfBirth = dateOfBirth;
-      this.address = address;
-    }
+class Person {
+  constructor(name, dateOfBirth, address) {
+    this.name = name;
+    this.dateOfBirth = new Date(dateOfBirth);
+    this.address = address;
   }
-  
-  // Создание двух объектов класса Person
-  const person1 = new Person('Женя', '01/02/1980', '12 Темерязево');
-  const person2 = new Person('Семен', '02/03/1991', '456 Тихомировский');
-  
-  // Создание нового объекта класса MyArray
-  const myArray = new MyArray();
-  
-  // Присваивание массива, содержащего объекты person1 и person2, свойству arr объекта myArray
-  myArray.arr = [person1, person2];
-  
-  // Получение элемента с id "result" из DOM
-  const resultElement = document.getElementById('result');
-  
-  // Присвоение элементу resultElement значения, возвращаемого методами getHtml(true) и getHtml(false) объекта myArray
-resultElement.innerHTML = "Список людей:" + myArray.getHtml(true);
-resultElement.innerHTML += "Детали каждого человека:" + myArray.getHtml(false);
+}
+
+const person1 = { name: 'Женя', dateOfBirth: new Date('01/02/1980'), address: '12 Темерязево' };
+const person2 = { name: 'Семен', dateOfBirth: new Date('02/03/1991'), address: '456 Тихомировский' };
+
+const myArray = {
+  arr: [person1, person2],
+  getHtml: function (numbered) {
+    let html = numbered ? '<ol>' : '<ul>';
+    for (let i = 0; i < this.arr.length; i++) {
+      const person = this.arr[i];
+      html += `<li>${person.name} - ${person.dateOfBirth.toLocaleDateString()} - ${person.address}</li>`;
+    }
+    html += numbered ? '</ol>' : '</ul>';
+    return html;
+  }
+};
+
+const resultElement = document.getElementById('result');
+resultElement.innerHTML = "Нумерованный список " + myArray.getHtml(true);
+resultElement.innerHTML += " Маркированный список " + myArray.getHtml(false);
